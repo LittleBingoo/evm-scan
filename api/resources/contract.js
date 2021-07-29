c1be796dcf7dbf161a358f3faff84f33fff3420a
LittleBingoo <zpksdhr@gmail.com>
2021-07-29 18:37:44 +0800

    api add creator and created transaction hash

--- api/resources/contract.js
+++ api/resources/contract.js
@@ -1,6 +1,12 @@
 const DB = require('../../models/index');
+const Transaction = DB.Transaction;
 
 async function serialize(contract) {
+
+    // contract creator
+    let transaction = await Transaction.findOne({
+        where: {created_contract_address_hash: contract.address_hash}
+    });
 
     return {
         name: contract.name,
@@ -8,7 +14,9 @@ async function serialize(contract) {
         abi: contract.abi,
         compiler_version: contract.compiler_version,
         optimization: contract.optimization,
-        contract_source_code: contract.contract_source_code
+        contract_source_code: contract.contract_source_code,
+        creator: transaction ? transaction.from_address_hash : '',
+        created_transaction_hash: transaction ? transaction.hash : '',
     }
 }
 
